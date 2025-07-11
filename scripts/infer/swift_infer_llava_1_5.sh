5ac496fdfff523b49ff53e1b33afc6b9e86a0905
qq <qianq9720@163.com>
2025-07-11 12:04:50 +0800

    训练&推理脚本修改

--- scripts/infer/swift_infer_llava_1_5.sh
+++ scripts/infer/swift_infer_llava_1_5.sh
@@ -3,7 +3,7 @@
 
 CUDA_VISIBLE_DEVICES="1,3"
 INFER_BACKEND="vllm"
-MODEL="/home/qianq/mycodes/llm/results/swift-llava-1.5-7b-hf/v0-20250710-154830/checkpoint-410"
+MODEL="/home/qianq/mycodes/llm/results/swift-projector-llava-1.5-7b-hf-epoch=5-lr=1e-5/v0-20250710-154830/checkpoint-250"
 DATASET="/home/qianq/mycodes/llm/data/image-text-to-text/LIDC-IDRI-MLLM-CLF-EN"
 MAX_BATCH_SIZE=16
 MAX_NEW_TOKENS=512
@@ -12,7 +12,7 @@ TOP_P=0.9
 VAL_DATASET_SAMPLE=500
 WRITE_BATCH_SIZE=64
 TENSOR_PARALLEL_SIZE=2
-RESULT_PATH="${MODEL}/vllm_inference_results.jsonl"
+RESULT_PATH="${MODEL}/inference_results.jsonl"
 METRIC="acc"
 
 echo "=== VLLM 高性能推理 ==="
