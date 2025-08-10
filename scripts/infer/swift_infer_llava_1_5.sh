0bd34e57e4f3ba64e779ce896c3aabe7d2f08f67
qq <qianq9720@163.com>
2025-08-10 15:27:38 +0800

    scripts save folder name change

--- scripts/infer/swift_infer_llava_1_5.sh
+++ scripts/infer/swift_infer_llava_1_5.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # VLLM 高性能推理
 
-CUDA_VISIBLE_DEVICES="2"
+CUDA_VISIBLE_DEVICES="3"
 INFER_BACKEND="vllm"
-# MODEL="/home/qianq/mycodes/llm/results/swift-projector-llava-1.5-7b-hf-EPOCH=5-LR=1e-6/v2-20250718-172038/checkpoint-410"
-MODEL="/home/qianq/model/llava-med-v1.5-mistral-7b"
+MODEL="/home/qianq/mycodes/llm/results/swift-projector-llava-med-v1.5-mistral-7b-EPOCH=5-LR=1e-6/v1-20250809-200334/checkpoint-820"
 
-DATASET="/home/qianq/mycodes/llm/data/image-text-to-text/lidc-clf-nodule-img/LIDC-IDRI-MLLM-CLF-EN-ATTRS"
+DATASET_NAME="LIDC-IDRI-MLLM-CLF-EN"
+DATASET="/home/qianq/data/image-text-to-text/lidc-clf-nodule-img/${DATASET_NAME}"
 MAX_BATCH_SIZE=16
 MAX_NEW_TOKENS=512
 TEMPERATURE=0.7 # sft模型的Temperature应该低一些 0.6
 TOP_P=0.9
 WRITE_BATCH_SIZE=64
 TENSOR_PARALLEL_SIZE=1
-RESULT_PATH="${MODEL}/inference_${DATASET}.jsonl"
+RESULT_PATH="${MODEL}/inference_${DATASET_NAME}.jsonl"
 METRIC="acc"
 
 echo "=== VLLM 高性能推理 ==="
@@ -22,10 +22,7 @@ echo "张量并行: $TENSOR_PARALLEL_SIZE"
 echo "大批量处理: $MAX_BATCH_SIZE"
 echo "======================"
 
-# TODO 进入数据集目录 ${DATASET} 并执行inference.py脚本
-cd ${DATASET} || exit 1
-
-python /home/qianq/mycodes/llm/inference.py \
+python inference.py \
     --cuda_visible_devices "$CUDA_VISIBLE_DEVICES" \
     --infer_backend "$INFER_BACKEND" \
     --model "$MODEL" \
